63ab4c0d801c3e8d22e6bb11a78f3a8fac1991fd
erfg12 <newgmobile@gmail.com>
2018-03-21 18:37:08 -0400

    fixed settings file

--- Discord extension/example0.cpp
+++ Discord extension/example0.cpp
@@ -16,31 +16,10 @@
 #include <unistd.h>
 #endif
 
-std::map<std::string, std::string> LoadConfig(std::string filename)
-{
-	std::ifstream input(filename); //The input stream
-	std::map<std::string, std::string> ans; //A map of key-value pairs in the file
-	while (input) //Keep on going as long as the file stream is good
-	{
-		std::string key; //The key
-		std::string value; //The value
-		std::getline(input, key, '='); //Read up to the : delimiter into key
-		std::getline(input, value, '\n'); //Read up to the newline into value
-		std::string::size_type pos1 = value.find_first_of("\""); //Find the first quote in the value
-		std::string::size_type pos2 = value.find_last_of("\""); //Find the last quote in the value
-		if (pos1 != std::string::npos && pos2 != std::string::npos && pos2 > pos1) //Check if the found positions are all valid
-		{
-			value = value.substr(pos1 + 1, pos2 - pos1 - 1); //Take a substring of the part between the quotes
-			ans[key] = value; //Store the result in the map
-		}
-	}
-	input.close(); //Close the file stream
-	return ans; //And return the result
-}
-
-std::map<std::string, std::string> settings = LoadConfig("settings.cfg");
+using namespace std;
 
 #define pipename "\\\\.\\pipe\\discord"
+const char *chanid;
 
 class myClientClass : public SleepyDiscord::DiscordClient {
 public:
@@ -67,7 +46,7 @@ public:
 						token = strtok(NULL, "|");
 					}
 					if (strstr("say", array[0]) != NULL) { //say command
-						sendMessage(settings.at("channelID"), array[1]);
+						sendMessage(chanid, array[1]);
 					}
 				}
 			}
@@ -105,6 +84,16 @@ public:
 };
 
 int main() {
+	string line;
+	ifstream myfile("channelid.cfg");
+	if (!myfile.is_open()) {
+		printf("missing channelid.cfg file!\n");
+		return 0;
+	}
+	getline(myfile, line);
+	chanid = line.c_str();
+	myfile.close();
+	
 	myClientClass client("NDIzNTcyMTA0Mzc0Mzg2NzAw.DYsR4A.VsdbYVBEYeVJMVyq5QXiYSRnWOM", 2);
 	client.run();
 }
